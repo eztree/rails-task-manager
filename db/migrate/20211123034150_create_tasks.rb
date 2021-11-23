0745e8ba7c6a6032236b754418f6c8f995e120db
eztree <huienlim@gmail.com>
2021-11-23 11:55:53 +0800

    Set up

--- db/migrate/20211123034150_create_tasks.rb
+++ db/migrate/20211123034150_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string, :title
-      t.text, :details
+      t.string :title
+      t.text :details
 
       t.timestamps
     end
